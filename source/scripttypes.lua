96f301916462a7f59687a6596b2910fe571ea2b9
zorggn <zorg@atw.hu>
2015-10-04 19:06:18 +0200

    minor clarification

--- source/scripttypes.lua
+++ source/scripttypes.lua
@@ -1,3 +1,4 @@
 -- The allowed script types
+-- Uses internal names taken from danmakufu.
 
 return {["character"] = true, ["single"] = true, ["plural"] = true, ["stage"] = true, ["package"] = true}
