154699b095f40a2e73be7341570ec0e6d3f44fed
zorggn <zorg@atw.hu>
2015-10-04 19:01:08 +0200

    More additions, edits and variable renaming

--- main.lua
+++ main.lua
@@ -70,15 +70,23 @@ love.load = function(arg)
 	-- Returns whether it should execute the main menu, a script, or a full game.
 	scriptType, scriptPath = init(arg)
 
-	-- Load in the necessary libraries, now that we know we can run the engine.
-	gs = require 'source.lib.vrld.hump.gamestate'
+	-- Load in the necessary modules, now that we know we can run the engine.
 
-	audio = require 'source.audio'
-	input = require 'source.input'
+	-- System
+	gamestates = require 'source.lib.vrld.hump.gamestate'
 	loadscript = require 'source.loadscript'
+	-- logger already loaded
 	ungerm = require 'source.ungerm'
 
-	-- Init the debug lib.
+	-- Concern
+	audio = require 'source.audio'
+	input = require 'source.input'
+	--layers = require 'source.layers'
+
+	-- Gameish
+	-- bullet = ...
+
+	-- Init modules that need to be.
 	ungerm.init()
 
 	-- Load the script, and start it!
@@ -109,7 +117,7 @@ end
 -- Tickrate-limited loop
 love.update = function(dt, tick)
 
-	local current = gs.current()
+	local current = gamestates.current()
 	if current.update then current:update(dt, tick) end
 
 	-- Needs to be at the end of the main update callback,
@@ -119,18 +127,18 @@ love.update = function(dt, tick)
 end
 
 -- After all scripts have done their updating, the code swaps out the old state with the new one,
--- so there isn't any nondeterminism by ordering.
+-- so there isn't any nondeterminism by ordering. (Think: Wrapped (Toroidal) 2D Cellular Automata)
 love.swap = function(dt, tick)
 
-	local current = gs.current()
+	local current = gamestates.current()
 	if current.swap then current:swap(dt, tick) end
 
 end
 
--- Every draw here should go to a canvas optimally.
+-- Framerate-limited; Every draw here should go to a canvas optimally.
 love.draw = function(df)
 
-	local current = gs.current()
+	local current = gamestates.current()
 	if current.draw then current:draw(df) end
 
 end
@@ -138,10 +146,12 @@ end
 -- After all scripts have done their drawing, the code renders all canvas layers to the screen.
 love.render = function(df)
 
-	local current = gs.current()
+	local current = gamestates.current()
 	if current.render then current:render(df) end
 
 	-- Debug data, printed on top of everything
+	-- Note, we are cheating a bit here by not drawing to a canvas
+	-- -> TODO: do draw to the topmost canvas since we want the screen to be resizable, and like this, it isn't.
 	ungerm.render(df)
 
 end
