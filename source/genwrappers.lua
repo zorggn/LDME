96ba4242b9658dbfb849abca96878baa6e197714
zorggn <zorg@atw.hu>
2015-10-04 19:04:01 +0200

    added generic wrapper list
    Holds what functions we export from various modules into the scripts'
    sandboxed environments.

---
+++ source/genwrappers.lua
@@ -0,0 +1,14 @@
+-- The list of generic wrappers that will be applied to any script type
+
+return {
+	'audio',            -- custom
+	'bit',              -- luaJIT
+	'collision',        -- custom
+	'datetime',         -- lua + love + custom
+	'debug',            -- lua + custom
+	'generic',          -- some of the lua internalities
+	'input',            -- custom
+	'math',             -- lua + love + custom
+	'misc',             -- lua + love + custom
+	'string',           -- lua + custom
+}
