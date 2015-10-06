6fb9d1ed8fc0f221378f2638a44a014ad6936b28
zorggn <zorg@atw.hu>
2015-10-06 21:01:35 +0200

    Made things more understandable
    if a bit less neat and packed...

--- source/debugoverlay.lua
+++ source/debugoverlay.lua
@@ -14,39 +14,32 @@ local lg = love.graphics
 
 local font
 
+local magnitudeCorrect = function(amount)
+	if     amount <       1024 then return amount             , 'B'
+	elseif amount <    1048576 then return amount /       1024, 'kB'
+	elseif amount < 1073741824 then return amount /    1048576, 'MB'
+	else                            return amount / 1073741824, 'GB'
+	end
+end
+
 -- This module
 
 local t = {}
 
 -- Callbacks
 
-t.render = function(ip)
+t.render = function(interpolation)
 
-	-- Locals for all the data we can get from löve
-	local width, height, hw, fh, aps, apsw, tps, tpsw, fps, fpsw, lag, lagw, l
-	local stats, dcs, dcw, csws, csww, txms, txmw, imgs, imgw, cnvs, cnvw, fnts, fntw, txm, txp
-
+	-- Holds stats about Löve's current graphic resources.
 	stats = love.graphics.getStats()
 
-	-- Correct for magnitude
-	if stats.texturememory < 1024 then
-		txm = stats.texturememory
-		txp = 'B'
-	elseif stats.texturememory < 1048576 then
-		txm = stats.texturememory / 1024
-		txp = 'kB'
-	elseif stats.texturememory < 1073741824 then
-		txm = stats.texturememory / 1048576
-		txp = 'MB'
-	else
-		txm = stats.texturememory / 1073741824
-		txp = 'GB'
-	end
+	-- Correct for magnitude (original lowercase style kept for consistency in this table)
+	stats.texturememory, stats.texturememoryprefix = magnitudeCorrect(stats.texturememory)
 
-	-- For displaying all this
-	width, height = lg.getDimensions()
-	hw = width  /  2
-	fh = height - 24
+	-- For positioning the data onto the screen.
+	local width, height = lg.getDimensions()
+	local halfWidth = width  /  2
+	local fontHeight = height - 24
 
 	lg.push('all')
 
@@ -55,44 +48,46 @@ t.render = function(ip)
 	lg.setColor(255,255,255,255)
 	lg.setBlendMode('alpha')
 
-	-- Lag percentage
-	l = (1-ip)*100
+	-- Amount of desync between update ticks and draw frames.
+	local lag = (1 - interpolation) * 100
 
 	-- Pre-format the strings
-	aps  = string.format("%3.2f a/s", 1/love.timer.getDelta())
-	apsw = font:getWidth("000000.00 aps")
-	tps  = string.format("%3.2f t/s",   love.timer.getTPS())
-	tpsw = font:getWidth("00.00 tps___")
-	fps  = string.format("%3.2f f/s",   love.timer.getFPS())
-	fpsw = font:getWidth("00.00 fps___")
-	lag  = string.format("%3.0f%% dt/t", l)
+	local atomsPerSecond       = string.format("%3.2f a/s", 1/love.timer.getDelta())
+	local atomsPerSecondWidth  = font:getWidth("000000.00 aps")
+	local ticksPerSecond       = string.format("%3.2f t/s",   love.timer.getTPS())
+	local ticksPerSecondWidth  = font:getWidth("00.00 tps___")
+	local framesPerSecond      = string.format("%3.2f f/s",   love.timer.getFPS())
+	local framesPerSecondWidth = font:getWidth("00.00 fps___")
+	local lagPercent           = string.format("%3.0f%% dt/t", lag)
 
-	dcs  = string.format("d(): %4d", stats.drawcalls)
-	dcw  = font:getWidth("d(): 0000")
-	csws = string.format("c<>: %4d", stats.canvasswitches)
-	csww = font:getWidth("c<>: 0000")
-	txms = string.format("tex: %10f " .. txp, txm)
-	txmw = font:getWidth("tex: 0000000000     " .. txp)
-	imgs = string.format("img: %4d", stats.images)
-	imgw = font:getWidth("img: 0000")
-	cnvs = string.format("cnv: %4d", stats.canvases)
-	cnvw = font:getWidth("cnv: 0000")
-	fnts = string.format("fnt: %4d", stats.fonts)
-	fntw = font:getWidth("fnt: 0000")
+	local drawCalls            = string.format("d(): %4d", stats.drawcalls)
+	local drawCallsWidth       = font:getWidth("d(): 0000")
+	local canvasSwitches       = string.format("c<>: %4d", stats.canvasswitches)
+	local canvasSwitchesWidth  = font:getWidth("c<>: 0000")
+	local textureMemory        = string.format("tex: %10f %s", stats.texturememoryprefix, stats.texturememory)
+	local textureMemoryWidth   = font:getWidth("tex: 0000000000     " .. stats.texturememoryprefix)
+	local imageCount           = string.format("img: %4d", stats.images)
+	local imageCountWidth      = font:getWidth("img: 0000")
+	local canvasCount          = string.format("cnv: %4d", stats.canvases)
+	local canvasCountWidth     = font:getWidth("cnv: 0000")
+	local fontCount            = string.format("fnt: %4d", stats.fonts)
+	local fontCountWidth       = font:getWidth("fnt: 0000")
 
 	-- Print the preformatted strings
-	lg.printf(aps, 0, fh, width-fpsw-tpsw-apsw, 'right')
-	lg.printf(tps, 0, fh, width-fpsw-tpsw,      'right')
-	lg.printf(fps, 0, fh, width-fpsw,           'right')
-	if l < 0 then lg.setColor(255,0,0,255) end
-	lg.printf(lag, 0, fh, width,                'right')
+	lg.printf(atomsPerSecond,  0, fontHeight, width-framesPerSecondWidth-ticksPerSecondWidth-atomsPerSecondWidth, 'right')
+	lg.printf(ticksPerSecond,  0, fontHeight, width-framesPerSecondWidth-ticksPerSecondWidth,                     'right')
+	lg.printf(framesPerSecond, 0, fontHeight, width-framesPerSecondWidth,                                         'right')
 
-	lg.printf(dcs,  0, fh-24, width-fntw-cnvw-imgw-txmw-csww, 'right')
-	lg.printf(csws, 0, fh-24, width-fntw-cnvw-imgw-txmw     , 'right')
-	lg.printf(txms, 0, fh-24, width-fntw-cnvw-imgw          , 'right')
-	lg.printf(imgs, 0, fh-24, width-fntw-cnvw               , 'right')
-	lg.printf(cnvs, 0, fh-24, width-fntw                    , 'right')
-	lg.printf(fnts, 0, fh-24, width                         , 'right')
+	if lag < 0 then lg.setColor(255,0,0,255) end
+	lg.printf(lag, 0, fontHeight, width, 'right')
+	lg.setColor(255,255,255,255)
+
+	lg.printf(drawCalls,      0, fontHeight-24, width-fontCountWidth-canvasCountWidth-imageCountWidth-textureMemoryWidth-canvasSwitchesWidth, 'right')
+	lg.printf(canvasSwitches, 0, fontHeight-24, width-fontCountWidth-canvasCountWidth-imageCountWidth-textureMemoryWidth                    , 'right')
+	lg.printf(textureMemory,  0, fontHeight-24, width-fontCountWidth-canvasCountWidth-imageCountWidth                                       , 'right')
+	lg.printf(imageCount,     0, fontHeight-24, width-fontCountWidth-canvasCountWidth                                                       , 'right')
+	lg.printf(canvasCount,    0, fontHeight-24, width-fontCountWidth                                                                        , 'right')
+	lg.printf(fontCount,      0, fontHeight-24, width                                                                                       , 'right')
 
 	lg.pop()
 
@@ -101,7 +96,9 @@ end
 -- Methods
 
 t.init = function()
+
 	font = love.graphics.newFont('assets/THSpatial.ttf',24)
+
 end
 
 ----------
